baf673dda0b833c0a73a2dd63c12fac17446e12d
Dadflip <92120636+dadflip@users.noreply.github.com>
2024-06-16 23:02:56 +0200

    Some modifications
    UI modifications

---
+++ OpenWeather-main/app/src/main/java/com/example/weather/ui/WeatherInfoBottomSheetFragment.kt
@@ -0,0 +1,17 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weather.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class WeatherInfoBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_info_bottom_sheet, container, false)
+    }
+}
